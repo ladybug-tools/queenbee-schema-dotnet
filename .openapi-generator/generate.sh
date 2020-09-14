956d27fb7abb2901aa3c64c9c2b349d0c3f8a1ce
MingboPeng <mingo1214@gmail.com>
2020-09-14 17:35:30 -0400

    fix(SDK): fix AnyOf "number" instead of "double"

--- .openapi-generator/generate.sh
+++ .openapi-generator/generate.sh
@@ -3,7 +3,7 @@
 python .openapi-generator/pre_gen_script.py 
 npx @openapitools/openapi-generator-cli generate -i "https://www.ladybug.tools/queenbee/_static/schemas/recipe-openapi.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
 python .openapi-generator/post_gen_script.py "https://www.ladybug.tools/queenbee/_static/schemas/recipe-openapi.json" 
-python .openapi-generator/update_assembly_version.py
+# python .openapi-generator/update_assembly_version.py
 
 
 
