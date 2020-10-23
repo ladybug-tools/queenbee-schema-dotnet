c0c603c9bae81c4cb151b4d5d5cbd271825dc45a
MingboPeng <mingo1214@gmail.com>
2020-10-23 18:00:29 -0400

    fix(gen): update generator

--- .openapi-generator/generate.sh
+++ .openapi-generator/generate.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+# install 
+npm install @openapitools/openapi-generator-cli
+
+# check version
+# npx @openapitools/openapi-generator-cli version-manager list
+# use 5.0.0
+
+# check version, clean up
 python .openapi-generator/pre_gen_script.py 
-npx @openapitools/openapi-generator-cli generate -i "openapi/recipe_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
+
+# generate
+npx @openapitools/openapi-generator-cli generate -i "openapi/recipe_inheritance.json" -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-generator/config.json 
+
+# fix anyof, clean up
 python .openapi-generator/post_gen_script.py "openapi/recipe_inheritance.json" 
-# python .openapi-generator/update_assembly_version.py
 
-
-
-npx @openapitools/openapi-generator-cli generate -i "openapi/recipe_inheritance.json"  -g csharp-netcore -o . -c .openapi-generator/config.json --skip-validate-spec
-
-npx @openapitools/openapi-generator-cli generate -i "openapi/recipe_inheritance.json" -t ".openapi-generator/templates/csharp" -g csharp -o . -c .openapi-generator/config.json --skip-validate-spec
-
+# build, test, pack
+dotnet build -c Release /nowarn:CS0472,CS0108,CS1591
+dotnet test -c Release
+dotnet pack .\\src\\QueenbeeSDK\\QueenbeeSDK.csproj -o ../../output -c Release --no-build
