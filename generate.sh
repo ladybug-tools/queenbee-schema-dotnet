c0f7abe8e00a483d92fa734cc0866368d5b8153c
MingboPeng <mingo1214@gmail.com>
2020-11-04 12:45:59 -0500

    doc(schema): update openapi schema

--- generate.sh
+++ generate.sh
@@ -1,8 +1,8 @@
-python3 .openapi-generator/pre_gen_script.py ".openapi-docs/model_inheritance.json"
+python3 .openapi-generator/pre_gen_script.py ".openapi-docs/queenbee_inheritance.json"
 
-npx @openapitools/openapi-generator-cli generate -i ".openapi-docs/model_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-config.json 
+npx @openapitools/openapi-generator-cli generate -i ".openapi-docs/queenbee_inheritance.json"  -t ".openapi-generator/templates/csharp" -g csharp -o . --skip-validate-spec -c .openapi-config.json 
 
-python3 .openapi-generator/post_gen_script.py ".openapi-docs/model_inheritance.json"
+python3 .openapi-generator/post_gen_script.py ".openapi-docs/queenbee_inheritance.json"
 
 python3 .openapi-generator/update_assembly_version.py
 
