5aca13882295c2d03f9060715920df59af7ac691
MingboPeng <mingo1214@gmail.com>
2020-12-16 22:14:10 -0500

    fix(job): add null check for Job.Arguments before adding argument to it

--- src/QueenbeeSDK/ManuallyAdded/Model/Job.cs
+++ src/QueenbeeSDK/ManuallyAdded/Model/Job.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 namespace QueenbeeSDK
 {
     public partial class Job
@@ -5,12 +7,22 @@ namespace QueenbeeSDK
         public void AddArgument(JobArgument arg)
         {
             if (Arguments == null) this.Arguments = new System.Collections.Generic.List<AnyOf<JobArgument, JobPathArgument>>();
-            this.Arguments.Add(arg);
+
+            var existing = this.Arguments.OfType<JobArgument>().FirstOrDefault(_ => _.Name == arg.Name);
+            if (existing == null)
+                this.Arguments.Add(arg);
+            else
+                existing.Value = arg.Value;
         }
         public void AddArgument(JobPathArgument arg)
         {
             if (Arguments == null) this.Arguments = new System.Collections.Generic.List<AnyOf<JobArgument, JobPathArgument>>();
-            this.Arguments.Add(arg);
+
+            var existing = this.Arguments.OfType<JobPathArgument>().FirstOrDefault(_ => _.Name == arg.Name);
+            if (existing == null)
+                this.Arguments.Add(arg);
+            else
+                existing.Source = arg.Source;
         }
     }
 }
