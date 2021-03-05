003314ccef417471edb3086a25eb8a81e7407b36
MingboPeng <mingo1214@gmail.com>
2021-03-05 00:15:01 -0500

    feat(IStep): add IStep

---
+++ src/QueenbeeSDK/ManuallyAdded/Interface/IStep.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace QueenbeeSDK.Interface.Io.Inputs
+{
+    public partial interface IStep : IoBase
+    {
+        List<AnyOf<DAGGenericInputAlias, DAGStringInputAlias, DAGIntegerInputAlias, DAGNumberInputAlias, DAGBooleanInputAlias, DAGFolderInputAlias, DAGFileInputAlias, DAGPathInputAlias, DAGArrayInputAlias, DAGJSONObjectInputAlias, DAGLinkedInputAlias>> Alias { get; set; }
+    }
+
+}
